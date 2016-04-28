22175383987002858f95484457bfdd990a1675db
jaggyluis <luis.jaggy@gmail.com>
2016-04-28 15:34:05 -0700

    refactor: building model

---
+++ sfo/src/js/model/IATA/airlines.js
@@ -0,0 +1,154 @@
+var airlines = [
+    {
+        "IATA": "3K",
+        "name": "Jetstar Asia"
+    },
+    {
+        "IATA": "9W",
+        "name": "Jet Airways/Air India"
+    },
+    {
+        "IATA": "AA",
+        "name": "American Airlines"
+    },
+    {
+        "IATA": "AF",
+        "name": "Air France"
+    },
+    {
+        "IATA": "AS",
+        "name": "Alaska Airlines"
+    },
+    {
+        "IATA": "B6",
+        "name": "JetBlue Airways"
+    },
+    {
+        "IATA": "BA",
+        "name": "British Airways"
+    },
+    {
+        "IATA": "CX",
+        "name": "Cathay Pacific"
+    },
+    {
+        "IATA": "CZ",
+        "name": "China Southern"
+    },
+    {
+        "IATA": "D7",
+        "name": "AirAsiaX"
+    },
+    {
+        "IATA": "DL",
+        "name": "Delta Air Lines"
+    },
+    {
+        "IATA": "EK",
+        "name": "Emirates"
+    },
+    {
+        "IATA": "EY",
+        "name": "Etihad Airways"
+    },
+    {
+        "IATA": "F9",
+        "name": "Frontier Airlines"
+    },
+    {
+        "IATA": "GA",
+        "name": "Garuda"
+    },
+    {
+        "IATA": "HA",
+        "name": "Hawaiian Airlines"
+    },
+    {
+        "IATA": "JL",
+        "name": "Japan Airlines"
+    },
+    {
+        "IATA": "JQ",
+        "name": "Jetstar (Australia)"
+    },
+    {
+        "IATA": "KL",
+        "name": "KLM Royal Dutch Airlines"
+    },
+    {
+        "IATA": "MH",
+        "name": "Malaysia Airlines"
+    },
+    {
+        "IATA": "MK",
+        "name": "Air Mauritius"
+    },
+    {
+        "IATA": "NC",
+        "name": "Cobham"
+    },
+    {
+        "IATA": "NZ",
+        "name": "Air New Zealand"
+    },
+    {
+        "IATA": "PR",
+        "name": "Philippines Airlines"
+    },
+    {
+        "IATA": "QF",
+        "name": "Qantas"
+    },
+    {
+        "IATA": "QQ",
+        "name": "Alliance Airlines"
+    },
+    {
+        "IATA": "QR",
+        "name": "Qatar Airways"
+    },
+    {
+        "IATA": "QZ",
+        "name": "AirAsia (Indonesia)"
+    },
+    {
+        "IATA": "SA",
+        "name": "South African Airways"
+    },
+    {
+        "IATA": "SQ",
+        "name": "Singapore Airlines"
+    },
+    {
+        "IATA": "SY",
+        "name": "MN Airlines"
+    },
+    {
+        "IATA": "TG",
+        "name": "Thai Airways"
+    },
+    {
+        "IATA": "TR",
+        "name": "Tigerair"
+    },
+    {
+        "IATA": "TZ",
+        "name": "Scoot"
+    },
+    {
+        "IATA": "UA",
+        "name": "United Airlines"
+    },
+    {
+        "IATA": "VA",
+        "name": "Virgin Australia"
+    },
+    {
+        "IATA": "VN",
+        "name": "Vietnam Airlines"
+    },
+    {
+        "IATA": "WN",
+        "name": "Southwest Airlines"
+    }
+];
